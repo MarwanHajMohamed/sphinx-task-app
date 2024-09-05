43c6371b7f12f8fbbc41a5f43aeef9ab7b5983d7
Marwan <2017159@brunel.ac.uk>
2024-09-05 16:38:12 +0100

    Modify Students JSON
    - Modified form in JSON file

--- src/Components/StudentTable/StudentTable.jsx
+++ src/Components/StudentTable/StudentTable.jsx
@@ -5,44 +5,6 @@ import students from "../../Data/students.json";
 export default function StudentTable() {
   const labels = ["Pupil", "Form", "SEND"];
 
-  const tableLabels = labels.map((label) => {
-    return (
-      <div className="label">
-        <div>{label}</div>
-        <div>
-          <i class="fa-solid fa-sort"></i>
-        </div>
-      </div>
-    );
-  });
-
-  const studentsList = students.map((student) => {
-    return (
-      <div className="students-table">
-        <div className="column">
-          <img
-            src={student.profile_pic}
-            className="profile-pic"
-            alt="profile_pic"
-          />
-          <div className="name">
-            {student.forename} {student.surname}
-          </div>
-        </div>
-        <div className="column">
-          <div className="form">{student.form}</div>
-          <div className="send">
-            {student.send ? (
-              <i class="fa-solid fa-check" style={{ color: "blue" }}></i>
-            ) : (
-              <i class="fa-solid fa-check" style={{ color: "lightgray" }}></i>
-            )}
-          </div>
-        </div>
-      </div>
-    );
-  });
-
   return (
     <div className="students-table-container">
       <table>
