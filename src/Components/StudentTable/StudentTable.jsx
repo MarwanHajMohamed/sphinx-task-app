0a1ed536a1e9271a7e7bb4a5c4ed1771dc5cdb25
Marwan <2017159@brunel.ac.uk>
2024-09-05 16:28:57 +0100

    Modify Table
    -Switched to table tags
    - Styled table properties

--- src/Components/StudentTable/StudentTable.jsx
+++ src/Components/StudentTable/StudentTable.jsx
@@ -5,16 +5,32 @@ import students from "../../Data/students.json";
 export default function StudentTable() {
   const labels = ["Pupil", "Form", "SEND"];
 
+  const tableLabels = labels.map((label) => {
+    return (
+      <div className="label">
+        <div>{label}</div>
+        <div>
+          <i class="fa-solid fa-sort"></i>
+        </div>
+      </div>
+    );
+  });
+
   const studentsList = students.map((student) => {
     return (
       <div className="students-table">
         <div className="column">
-          <img src={student.profile_pic} className="profile-pic" />
+          <img
+            src={student.profile_pic}
+            className="profile-pic"
+            alt="profile_pic"
+          />
           <div className="name">
             {student.forename} {student.surname}
           </div>
         </div>
         <div className="column">
+          <div className="form">{student.form}</div>
           <div className="send">
             {student.send ? (
               <i class="fa-solid fa-check" style={{ color: "blue" }}></i>
@@ -27,23 +43,50 @@ export default function StudentTable() {
     );
   });
 
-  const tableLabels = labels.map((label) => {
-    return (
-      <div className="label">
-        <div>{label}</div>
-        <div>
-          <i class="fa-solid fa-sort"></i>
-        </div>
-      </div>
-    );
-  });
-
   return (
     <div className="students-table-container">
-      <div className="wrapper">
-        <div className="labels">{tableLabels}</div>
-        <div className="students">{studentsList}</div>
-      </div>
+      <table>
+        <thead>
+          <tr>
+            {labels.map((label) => {
+              return (
+                <th>
+                  <div className="label-container">
+                    <span>{label}</span>
+                    <div className="sort">
+                      <i class="fa-solid fa-sort-up"></i>
+                      <i class="fa-solid fa-sort-down"></i>
+                    </div>
+                  </div>
+                </th>
+              );
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {students.map((student) => {
+            return (
+              <tr>
+                <td className="row one">
+                  <img src={student.profile_pic} alt="profile_pic" />
+                  {student.forename} {student.surname}
+                </td>
+                <td>{student.form}</td>
+                <td>
+                  {student.send ? (
+                    <i class="fa-solid fa-check" style={{ color: "blue" }}></i>
+                  ) : (
+                    <i
+                      class="fa-solid fa-check"
+                      style={{ color: "lightgray" }}
+                    ></i>
+                  )}
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
