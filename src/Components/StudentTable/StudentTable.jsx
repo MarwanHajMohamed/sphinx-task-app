c082c9940864be731fe1dab0352bab492c4c17b5
Marwan <2017159@brunel.ac.uk>
2024-09-06 15:28:10 +0100

    Add Error Handling
    - Added error handling when there are no students found

--- src/Components/StudentTable/StudentTable.jsx
+++ src/Components/StudentTable/StudentTable.jsx
@@ -94,80 +94,91 @@ export default function StudentTable() {
   return (
     <div className="students-table-container">
       <SearchBar handleSearch={handleSearch} />
-      <table>
-        <thead>
-          <tr>
-            {headers.map((header) => {
-              return (
-                <th key={header} onClick={() => sortData(header)}>
-                  <div className="header-container">
-                    <span>{header}</span>
-                    <div className="sort">
-                      <i
-                        class={`fa-solid fa-sort-up ${
-                          sort.key === header && sort.direction === "ascending"
-                            ? "active-icon"
-                            : ""
-                        }`}
-                        style={{
-                          color:
-                            sort.key === header &&
-                            sort.direction === "ascending"
-                              ? "blue"
-                              : "rgb(179, 179, 179)",
-                        }}
-                      ></i>
-                      <i
-                        class={`fa-solid fa-sort-down ${
-                          sort.key === header && sort.direction === "descending"
-                            ? "active-icon"
-                            : ""
-                        }`}
-                        style={{
-                          color:
-                            sort.key === header &&
-                            sort.direction === "descending"
-                              ? "blue"
-                              : "rgb(179, 179, 179)",
-                        }}
-                      ></i>
-                    </div>
-                  </div>
-                </th>
-              );
-            })}
-          </tr>
-        </thead>
-        <tbody>
-          {currentStudents.map((student) => {
-            return (
-              <tr onClick={() => navigate(`/student/${student.id}`)}>
-                <td>
-                  <img src={student.profile_pic} alt="profile_pic" />
-                  {student.forename} {student.surname}
-                </td>
-                <td>{student.form}</td>
-                <td>
-                  {student.send ? (
-                    <i class="fa-solid fa-check" style={{ color: "blue" }}></i>
-                  ) : (
-                    <i
-                      class="fa-solid fa-check"
-                      style={{ color: "rgb(232, 232, 232)" }}
-                    ></i>
-                  )}
-                </td>
+      {currentStudents.length > 0 ? (
+        <>
+          <table>
+            <thead>
+              <tr>
+                {headers.map((header) => {
+                  return (
+                    <th key={header} onClick={() => sortData(header)}>
+                      <div className="header-container">
+                        <span>{header}</span>
+                        <div className="sort">
+                          <i
+                            class={`fa-solid fa-sort-up ${
+                              sort.key === header &&
+                              sort.direction === "ascending"
+                                ? "active-icon"
+                                : ""
+                            }`}
+                            style={{
+                              color:
+                                sort.key === header &&
+                                sort.direction === "ascending"
+                                  ? "blue"
+                                  : "rgb(179, 179, 179)",
+                            }}
+                          ></i>
+                          <i
+                            class={`fa-solid fa-sort-down ${
+                              sort.key === header &&
+                              sort.direction === "descending"
+                                ? "active-icon"
+                                : ""
+                            }`}
+                            style={{
+                              color:
+                                sort.key === header &&
+                                sort.direction === "descending"
+                                  ? "blue"
+                                  : "rgb(179, 179, 179)",
+                            }}
+                          ></i>
+                        </div>
+                      </div>
+                    </th>
+                  );
+                })}
               </tr>
-            );
-          })}
-        </tbody>
-      </table>
-      <Pagination
-        rowsPerPage={rowsPerPage}
-        totalRows={students.length}
-        paginate={paginate}
-        currentPage={currentPage}
-      />
+            </thead>
+            <tbody>
+              {currentStudents.map((student) => {
+                return (
+                  <tr onClick={() => navigate(`/student/${student.id}`)}>
+                    <td>
+                      <img src={student.profile_pic} alt="profile_pic" />
+                      {student.forename} {student.surname}
+                    </td>
+                    <td>{student.form}</td>
+                    <td>
+                      {student.send ? (
+                        <i
+                          class="fa-solid fa-check"
+                          style={{ color: "blue" }}
+                        ></i>
+                      ) : (
+                        <i
+                          class="fa-solid fa-check"
+                          style={{ color: "rgb(232, 232, 232)" }}
+                        ></i>
+                      )}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+          <Pagination
+            rowsPerPage={rowsPerPage}
+            totalRows={students.length}
+            paginate={paginate}
+            currentPage={currentPage}
+          />
+        </>
+      ) : (
+        "No students found"
+      )}
     </div>
   );
 }
